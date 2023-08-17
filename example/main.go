e1f177c7609ada8b46ec49f3bb4183cef0955388
Pedro Barbosa <jpedro.barbosa@gmail.com>
2023-08-18 00:49:32 +0200

    I'M PUSHING.

--- example/main.go
+++ example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/jpedro/request"
 )
@@ -19,6 +20,12 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Status code: %d\n", res.StatusCode)
-	fmt.Printf("Body length: %d bytes\n", len(res.Body))
+	fmt.Printf("\033[32;1mStatus code:\033[0m %d\n", res.StatusCode)
+	fmt.Printf("\033[32;1mBody length:\033[0m %d bytes\n", len(res.Body))
+	fmt.Printf("\033[32;1mBody sample:\033[0m\n")
+	lines := strings.Split(string(res.Body[0:1000]), "\n")
+	for i, line := range lines {
+		fmt.Printf("\033[2m %5d │\033[0m %s\033[0m\n", i, line)
+	}
+	// fmt.Printf("\033[32;1m¶\033[0m\n",)
 }
